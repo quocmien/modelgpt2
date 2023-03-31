1cad7f3864480b111e5765ff0d3a4fd1e5978f02
Quoc Mien <quocmienn@gmail.com>
2023-03-31 20:33:13 +0700

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-import  { config } from "dotenv"
+import { config } from "dotenv"
 config()
 
 import { Configuration, OpenAIApi } from "openai"
 import readline from "readline"
+import io from "socket.io-client"
 
 const openai = new OpenAIApi(new Configuration({
   apiKey: process.env.API_KEY
 }))
+
+const socket = io("http://localhost:4000") // Thay đổi địa chỉ và cổng phù hợp với ứng dụng của bạn
+
+socket.on("message", async message => {
+  const res = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: message }],
+  })
+  console.log(res.data.choices[0].message.content)
+})
 
 const userInterface = readline.createInterface({
   input: process.stdin,
@@ -14,13 +25,7 @@ const userInterface = readline.createInterface({
 })
 
 userInterface.prompt()
-userInterface.on("line", async input => {
-  const res = await openai
-  .createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
-  })
-  console.log(res.data.choices[0].message.content)
+userInterface.on("line", input => {
+  socket.emit("message", input)
   userInterface.prompt()
 })
-
